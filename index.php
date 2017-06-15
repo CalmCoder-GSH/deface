33bf13c62d238218981af045b0a076e1cef20db8
Mochamad Faishal <mochamadfaishal1311@gmail.com>
2017-06-15 21:09:50 +0700

    Create index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<html>
+<head>
 <title>Hacked</title>
-<h1>Touched By ./CalmCoder - Garuda Security Hacker<br>NoNameUser - Error Violence</h1><br>
-<h3>Sorry Pak Lupa Back Up Index :(</h3>
+<meta property="og:title" content="Touched By ./CalmCoder">
+<meta property="description" content="Just Random And Just For Fun"> 
+<meta property="og:author" content="./CalmCoder">
+<meta charset="UTF-8">
+</head> 
+<h1>Touched By ./CalmCoder - Garuda Security Hacker</h1><br>
+</html>
